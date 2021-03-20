a64e7675a0394b54e17f2cc5856bc4dd9d5c18b5
JohansenJunias20 <johansen.gumbal@gmail.com>
2021-03-20 21:55:29 +0700

    install body parser, adding ssl

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const ws = require('ws');
-const express = require('express');
-const https = require('https');
+const WebSocket = require('ws');
 
 var fs = require('fs');
 var https = require('https');
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+var privateKey = fs.readFileSync('/etc/letsencrypt/live/rust-sby.xyz/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/rust-sby.xyz/cert.pem', 'utf8');
 
-var credentials = {key: privateKey, cert: certificate};
+var credentials = { key: privateKey, cert: certificate };
 var express = require('express');
 var app = express();
 
 // your express configuration here
 
 var httpsServer = https.createServer(credentials, app);
+
+const wss = new WebSocket.Server({ server: httpsServer });
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+
+    ws.send('something');
+});
 
 httpsServer.listen(8443);
