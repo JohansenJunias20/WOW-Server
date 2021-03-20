c7f3d94bddbb95d58494f06d0d79c1e92138a9db
JohansenJunias20 <johansen.gumbal@gmail.com>
2021-03-20 22:24:41 +0700

    adding authorization logic code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const WebSocket = require('ws');
 const validator = require('validator');
+var mysql = require('mysql');
 var fs = require('fs');
 var https = require('https');
 var privateKey = fs.readFileSync('/etc/letsencrypt/live/rust-sby.xyz/privkey.pem', 'utf8');
@@ -20,7 +21,6 @@ wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
     });
-
     ws.send('something');
 });
 
@@ -31,12 +31,38 @@ app.use(bodyParser.json())
 
 // HTTPS LISTENER
 // *-----------------------------------------------------*
-app.post("/authorization", (req, res) => {
+app.post("/authorization", (req, resHttp) => {
     console.log(req.body);
     const message = req.body;
     const username = message.username;
     const password = message.password;
-    res.send("Hello");
+
+    var response = {
+        status: "unknown",
+        reason: undefined,
+        data: {
+            WOW: undefined,
+            LED: undefined
+
+        }
+    }
+    connection.query("SELECT * FROM users where username = ? and password = ?", [username, password],
+        (err, res) => {
+            if (err)
+                throw err;
+            if (res.length != 0) {
+                //ada
+                response.status = "success";
+                response.data.WOW = res[0]["WOW"];
+                response.data.LED = res[0]["LED"];
+                resHttp.send(response);
+            }
+            else{
+                response.status = "failed";
+                response.reason = "no username or password";
+                resHttp.send(response);
+            }
+        })
 })
 app.get("/", (req, res) => {
     res.send("Hello");
@@ -44,3 +70,34 @@ app.get("/", (req, res) => {
 // WSS LISTENER
 // *-----------------------------------------------------*
 httpsServer.listen(8443);
+
+
+
+var dbConfig = {
+    host: 'localhost',
+    user: 'junias20',
+    password: 'Jjdvih0506',
+    database: 'WOW',
+    port: "3306"
+};
+
+var connection;
+function handleDisconnect() {
+    connection = mysql.createConnection(dbConfig);  // Recreate the connection, since the old one cannot be reused.
+    connection.connect(function onConnect(err) {   // The server is either down
+        if (err) {                                  // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 10000);    // We introduce a delay before attempting to reconnect,
+        }                                           // to avoid a hot loop, and to allow our node script to
+    });                                             // process asynchronous requests in the meantime.
+    // If you're also serving http, display a 503 error.
+    connection.on('error', function onError(err) {
+        console.log('db error', err);
+        if (err.code == 'PROTOCOL_CONNECTION_LOST') {   // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                        // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+handleDisconnect();
